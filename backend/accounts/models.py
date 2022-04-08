90f5cc5fbf7cf3ec13e8c3fc3ca653e38d91bb41
memegauste <memegauste@gmail.com>
2022-04-08 05:15:19 +0200

    isort

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,7 +1,8 @@
 """Init project."""
 # Django
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
