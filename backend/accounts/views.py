2d401c09f31816b59c09e5f51e4c4bb47da75b22
memegauste <memegauste@gmail.com>
2022-04-08 05:33:42 +0200

    PDF Gen code portage

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,14 +1,21 @@
 """Views file."""
+# Standard Library
+import io
+
 # Django
 from django.contrib.auth import get_user_model
+from django.core.files.base import ContentFile
+from django.http import FileResponse
 from django.http import Http404
 # Create your views here.
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.views.generic import TemplateView
 
 # 3rd-party
 from rest_framework import permissions
 from rest_framework.generics import CreateAPIView
+from weasyprint import HTML
 
 # Project
 from backend.accounts.serializers import UserSerializer
@@ -52,3 +59,25 @@ class CreateUserView(CreateAPIView):
         permissions.AllowAny,
     ]
     serializer_class = UserSerializer
+
+
+def pdf_gen_code(request):
+    """PDF gen code with weasyprint."""
+    try:
+        template = render_to_string(
+            'pdf_template.html',
+            {},
+        )
+        html = HTML(
+            string=template,
+            base_url=request.build_absolute_uri(),
+        )
+        file_buffer = io.BytesIO(html.write_pdf())
+        response = FileResponse(
+            ContentFile(file_buffer.getvalue(), 'example.pdf'),
+            as_attachment=True, content_type='applicatiopn/pdf',
+        )
+        return response
+    except BaseException:
+        pass
+    raise Http404
