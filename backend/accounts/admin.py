554ae4d5f6116376d3958b23ed86b08b8db1724b
PSzczepanski1996 <p.szczepanski996@gmail.com>
2022-04-08 04:56:27 +0200

    Partial changes

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,4 +1,20 @@
+"""Admin file."""
 # Django
 from django.contrib import admin
 
 # Register your models here.
+from backend.accounts.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    """Custom User admin."""
+
+    list_display = [
+        '__str__',
+        'account_type',
+        'nr_index',
+    ]
+    search_fields = [
+        'nr_index',
+    ]
