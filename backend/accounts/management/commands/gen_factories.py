e1093096b671fd36489139f9a5e507163543d072
PSzczepanski996 <pszczepanski@webtechnika.pl>
2022-05-03 19:48:37 +0200

    News views for chat

---
+++ backend/accounts/management/commands/gen_factories.py
@@ -0,0 +1,24 @@
+"""Gen based factories."""
+import factory
+from django.utils import timezone
+from factory.django import DjangoModelFactory
+from backend.accounts.models import CustomUser
+
+
+class GenerateUsersFactory(DjangoModelFactory):
+    """Factory for User model."""
+
+    class Meta:  # noqa: D106
+
+        model = CustomUser
+        django_get_or_create = ('username',)
+
+    username = factory.Sequence(lambda n: f'User {n}')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    email = factory.Faker('email')
+    is_staff = False
+    is_active = True
+    date_joined = timezone.now()
+    account_type = CustomUser.STUDENT
+    nr_index = factory.Sequence(lambda n: str(n))
---
+++ backend/accounts/management/commands/generate_students.py
@@ -0,0 +1,24 @@
+"""Generate students command."""
+
+# Django
+from django.core.management.base import BaseCommand
+
+from .gen_factories import GenerateUsersFactory
+
+
+class Command(BaseCommand):
+    """Command."""
+
+    help = 'Generates predefined amount of users as students instances.'
+
+    def add_arguments(self, parser):  # noqa: D102
+        parser.add_argument('amount', type=int)
+
+    def handle(self, *args, **options):
+        """Handle command."""
+        students_amount = options['amount']
+        GenerateUsersFactory.create_batch(size=students_amount)
+        self.stdout.write(self.style.SUCCESS(
+                f'Generated {students_amount} students!',
+            ),
+        )
