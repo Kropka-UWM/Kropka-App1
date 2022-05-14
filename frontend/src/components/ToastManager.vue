e81018400e6563237c26f7ba4f28a93a2bea2e20
Grzegorz Choczaj <grzegorz.choczaj@gonnabe.pl>
2022-05-14 16:47:04 +0200

    Adding other logic used with login, dark mode and updating packpages

---
+++ frontend/src/components/ToastManager.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import Toast from "./common/Toast.vue";
+
+import { key } from "@/store";
+import { useStore } from "vuex";
+
+const store = useStore(key);
+
+const toasts = computed(() => store.state.toasts);
+</script>
+
+<template>
+  <div class="toast-container position-absolute top-0 end-0 m-4">
+    <Toast
+      v-for="(toast, index) in toasts"
+      :text-class="`text-white`"
+      :toast-class="toast.toastClass"
+      :key="index"
+    >
+      {{ toast.text }}
+    </Toast>
+  </div>
+</template>
---
+++ frontend/src/components/userpanel/Sidebar.vue
@@ -0,0 +1,122 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { key } from "@/store";
+import { useStore } from "vuex";
+
+const store = useStore(key);
+
+function logout() {
+  store.dispatch("logout");
+}
+
+let dayMode = store.state.darkMode;
+
+function changeMode() {
+  store.commit("SET_DAY_MODE");
+}
+</script>
+
+<template>
+  <div
+    class="d-flex flex-column flex-shrink-0 p-3 bg-light"
+    style="width: 280px"
+  >
+    <a
+      href="/"
+      class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
+    >
+      <svg class="bi me-2" width="40" height="32">
+        <use xlink:href="/@/assets/icons/bootstrap-icons.svg#bootstrap"></use>
+      </svg>
+      <span class="fs-4">Sidebar</span>
+    </a>
+    <hr />
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="#" class="nav-link active" aria-current="page">
+          <svg class="bi me-2" width="16" height="16">
+            <use
+              xlink:href="@/assets/icons/bootstrap-icons.svg#bootstrap"
+            ></use>
+          </svg>
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link link-dark">
+          <svg class="bi me-2" width="16" height="16">
+            <use
+              xlink:href="@/assets/icons/bootstrap-icons.svg#speedometer2"
+            ></use>
+          </svg>
+          Dashboard
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link link-dark">
+          <svg class="bi me-2" width="16" height="16">
+            <use xlink:href="@/assets/icons/bootstrap-icons.svg#table"></use>
+          </svg>
+          Orders
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link link-dark">
+          <svg class="bi me-2" width="16" height="16">
+            <use xlink:href="@/assets/icons/bootstrap-icons.svg#grid"></use>
+          </svg>
+          Products
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link link-dark">
+          <svg class="bi me-2" width="16" height="16">
+            <use
+              xlink:href="@/assets/icons/bootstrap-icons.svg#people-circle"
+            ></use>
+          </svg>
+          Customers
+        </a>
+      </li>
+    </ul>
+    <div class="form-check form-switch">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        id="flexSwitchCheckDefault"
+        v-model="dayMode"
+        @change="changeMode"
+      />
+      <label class="form-check-label" for="flexSwitchCheckDefault"
+        >Day/Light mode</label
+      >
+    </div>
+    <hr />
+    <div class="dropdown">
+      <a
+        href="#"
+        class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle"
+        id="dropdownUser2"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        <img
+          src="https://github.com/mdo.png"
+          alt=""
+          width="32"
+          height="32"
+          class="rounded-circle me-2"
+        />
+        <strong>{{ store.state.user.name }}</strong>
+      </a>
+      <ul
+        class="dropdown-menu text-small shadow"
+        aria-labelledby="dropdownUser2"
+        style=""
+      >
+        <li><hr class="dropdown-divider" /></li>
+        <li><a class="dropdown-item" href="#" @click="logout">Sign out</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
