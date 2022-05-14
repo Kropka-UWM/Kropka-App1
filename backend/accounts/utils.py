4fabd005f31b83212ee9811481c73d00a0579dfe
memegauste <memegauste@gmail.com>
2022-05-14 23:04:52 +0200

    General cleanup and small fixes

--- backend/accounts/utils.py
+++ backend/accounts/utils.py
@@ -10,10 +10,10 @@ from backend.project import settings
 def send_email_to_user(user, title, context, template_name=None):
     """Send email contains order-data to client."""
     if not template_name:
-        template = 'pdf_template.html'
+        template_name = 'pdf_template.html'
     msg = EmailMultiAlternatives(
         title,
-        render_to_string(template, context),
+        render_to_string(template_name, context),
         settings.DEFAULT_FROM_EMAIL,
         [user.email],
     )
