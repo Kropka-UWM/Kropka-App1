e32e156da90a9689eabbf500b14efe86bc9dba7a
PSzczepanski996 <pszczepanski@webtechnika.pl>
2022-05-20 18:35:26 +0200

    Add generation of PDF initial functionality

--- backend/accounts/management/commands/generate_user_companies.py
+++ backend/accounts/management/commands/generate_user_companies.py
@@ -1,4 +1,4 @@
-"""Generate leaders command."""
+"""Generate companies command."""
 
 # Django
 from django.core.management.base import BaseCommand
@@ -11,19 +11,19 @@ from .gen_factories import GenerateUsersFactory
 class Command(BaseCommand):
     """Command."""
 
-    help = 'Generates predefined amount of users as leaders instances.'
+    help = 'Generates predefined amount of users as companies instances.'
 
     def add_arguments(self, parser):  # noqa: D102
         parser.add_argument('amount', type=int)
 
     def handle(self, *args, **options):
         """Handle command."""
-        leaders_amount = options['amount']
+        companies_amount = options['amount']
         GenerateUsersFactory.create_batch(
-            size=leaders_amount,
-            account_type=CustomUser.LEADER,
+            size=companies_amount,
+            account_type=CustomUser.COMPANY,
         )
         self.stdout.write(self.style.SUCCESS(
-                f'Generated {leaders_amount} leaders!',
+                f'Generated {companies_amount} companies!',
             ),
         )
